c4d330bcf74a8f2591f37615697674914b56867a
hasan-huda <hasanhuda037@gmail.com>
2024-06-12 17:30:21 -0400

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Dashboard() {
+  //get user here from session or the return from signin with google or login
+  //setup admin email credentials and details for an admin page
+  return <>
+  </>
+}
